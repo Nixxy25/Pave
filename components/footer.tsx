b369c98531dbf1e69ab0d0ca1d1fc24ce8429f4f
Nixxy25 <tellaoyinkansola25@gmail.com>
2026-02-02 12:03:03 +0100

    style: update the footer UI

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <div className="flex flex-col min-h-[60vh] px-24 max-lg:px-8 py-4">
 
         <div className="flex-1 flex items-center justify-center">
-          <div className="poppins-regular  text-8xl sm:text-8xl md:text-9xl lg:text-[12rem] text-white tracking-tight leading-none">
+          <div className="poppins-regular  text-8xl sm:text-8xl md:text-9xl lg:text-[16rem] text-white tracking-tight leading-none">
             PAVE
           </div>
         </div>
@@ -22,23 +22,23 @@ export function Footer() {
                 href="https://x.com/paveng_"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="poppins-light text-xl flex items-center gap-2 hover:text-white transition-colors"
+                className="poppins-light max-sm:text-base text-xl flex items-center gap-2 hover:text-white transition-colors"
               >
                 Twitter
                 <ArrowUpRight className="w-5 h-5" />
               </a>
               <a
-                href="https://discord.com"
+                // href="https://discord.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="poppins-light text-xl flex items-center gap-2 hover:text-white transition-colors"
+                className="poppins-light max-sm:text-base text-xl flex items-center gap-2 hover:text-white transition-colors"
               >
                 Discord
                 <ArrowUpRight className="w-5 h-5" />
               </a>
               <a
                 // href="mailto:hello@pave.com"
-                className="poppins-light text-xl hover:text-white transition-colors"
+                className="poppins-light max-sm:text-base text-xl hover:text-white transition-colors"
               >
                 Email
               </a>
